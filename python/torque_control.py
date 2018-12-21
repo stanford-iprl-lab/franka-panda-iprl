d36704b6914b44f37c70a00ae99737acf19a3d48
Michelle Lee <amichlee@gmail.com>
2018-12-20 17:02:03 -0800

    Fix redis key byte decoding

--- python/torque_control.py
+++ python/torque_control.py
@@ -10,7 +10,8 @@ def main():
     redis_db.set("franka_panda::control::kp_joint", kp_joint)
     redis_db.set("franka_panda::control::kv_joint", kv_joint)
 
-    q_des = np.array([float(n) for n in redis_db.get("franka_panda::sensor::q").strip().split(" ")])
+    print(redis_db.get("franka_panda::sensor::q"))
+    q_des = np.array(list(map(float, redis_db.get("franka_panda::sensor::q").decode("utf-8").strip().split(" "))))
 
     while True:
         time.sleep(0.001)
@@ -18,14 +19,14 @@ def main():
         kp_joint = float(redis_db.get("franka_panda::control::kp_joint"))
         kv_joint = float(redis_db.get("franka_panda::control::kv_joint"))
 
-        q = np.array(list(map(float, redis_db.get("franka_panda::sensor::q").strip().split(" "))))
-        dq = np.array(list(map(float, redis_db.get("franka_panda::sensor::dq").strip().split(" "))))
-        tau = np.array(list(map(float, redis_db.get("franka_panda::sensor::tau").strip().split(" "))))
-        dtau = np.array(list(map(float, redis_db.get("franka_panda::sensor::dtau").strip().split(" "))))
+        q = np.array(list(map(float, redis_db.get("franka_panda::sensor::q").decode("utf-8").strip().split(" "))))
+        dq = np.array(list(map(float, redis_db.get("franka_panda::sensor::dq").decode("utf-8").strip().split(" "))))
+        tau = np.array(list(map(float, redis_db.get("franka_panda::sensor::tau").decode("utf-8").strip().split(" "))))
+        dtau = np.array(list(map(float, redis_db.get("franka_panda::sensor::dtau").decode("utf-8").strip().split(" "))))
 
-        A = np.array([list(map(float, row.strip().split(" "))) for row in redis_db.get("franka_panda::model::mass_matrix").strip().split(";")])
-        V = np.array(list(map(float, redis_db.get("franka_panda::model::coriolis").strip().split(" "))))
-        G = np.array(list(map(float, redis_db.get("franka_panda::model::gravity").strip().split(" "))))
+        A = np.array([list(map(float, row.strip().split(" "))) for row in redis_db.get("franka_panda::model::mass_matrix").decode("utf-8").strip().split(";")])
+        V = np.array(list(map(float, redis_db.get("franka_panda::model::coriolis").decode("utf-8").strip().split(" "))))
+        G = np.array(list(map(float, redis_db.get("franka_panda::model::gravity").decode("utf-8").strip().split(" "))))
 
         ddq = -kp_joint * (q - q_des) - kv_joint * dq
 
@@ -39,7 +40,7 @@ def main():
         print("V: ", V)
         print("G: ", G)
 
-        redis_db.set("franka_panda::control::tau", " ".join(map(str, ab.q.tolist()))
+        redis_db.set("franka_panda::control::tau", " ".join(map(str, ab.q.tolist())))
         redis_db.set("franka_panda::control::mode", "torque")
 
 if __name__ == "__main__":
