12afba8bd5d1d9887dcf8a3baa278fa04be7f222
Toki Migimatsu <toki.migimatsu@gmail.com>
2019-01-09 18:33:00 -0800

    Add FrankaPanda header

---
+++ include/FrankaPanda/FrankaPanda.h
@@ -0,0 +1,16 @@
+/**
+ * FrankaPanda.h
+ *
+ * Copyright 2018. All Rights Reserved.
+ * Stanford IPRL
+ *
+ * Created: January 09, 2018
+ * Authors: Toki Migimatsu
+ */
+
+#ifndef FRANKA_PANDA_H_
+#define FRANKA_PANDA_H_
+
+#include "FrankaPanda/model.h"
+
+#endif  // FRANKA_PANDA_H_
